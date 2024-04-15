192e6a375f7261849c8699aaa3bba539210cef84
SomeBody <maxim.karaichev@saltpepper.ru>
2024-04-15 19:28:38 +0300

    1

--- config/settings.py
+++ config/settings.py
@@ -105,7 +105,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
-LOGIN_REDIRECT_URL = '/'
+
 LOGOUT_REDIRECT_URL = '/'
 
 CELERY_BROKER_URL = "redis://localhost:6379"
